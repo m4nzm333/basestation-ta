052437f563357e170f1ac642e5208487cbf11f58
m4nzm333 <irman.mashuri@gmail.com>
2020-02-03 01:59:46 +0800

    Multi Topic to Subscribe

--- ReadMQTTClient.py
+++ ReadMQTTClient.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2020 m4nzm333
+# Source code for receive data from Sensor Node
+# Source : https://github.com/m4nzm333/basestation-ta
+# Ask a question, please contact:
+# e-mail    : irman.mashuri@gmail.com
+# hp/wa     : +6285396397688
+
 import paho.mqtt.client as mqtt
 import datetime as datetime
 
@@ -5,20 +12,24 @@ import datetime as datetime
 MQTT_Broker = "192.168.1.9"
 MQTT_Port = 1883
 Keep_Alive_Interval = 45
-MQTT_Topic = "Temperatur"
+# MQTT multi topic for subscribe
+MQTT_Topic = [("temperature", 0),("pressure", 0),("hummidity", 0)]
 
-mqttc = mqtt.Client("Detektor Temperatur")
+mqttc = mqtt.Client("Basestation-0001")
 # Read Data Function
 def on_message(mosq, obj, msg):
     print("============================")
-    print("MQTT Data Received On " + str(datetime.datetime.now()))
-    print("Data : " + msg.payload)
-    print("============================")
+    print("Timestamp : " + str(datetime.datetime.now()))
+    print("Topic     : " + msg.topic)
+    print("Payload   : " + msg.payload.decode('utf-8'))
     print("\n")
+    # TODO : Add saveToArchive funtion
 
 def on_subscribe(mosq, obj, mid, granted_qos):
+    print(str(mosq.topic))
     pass
 
+# Show error if connection unsuccessful
 def on_connect(client, userdata, flag, rc):
     if rc==0:
         print("Connection successful\n")
@@ -33,7 +44,6 @@ def on_connect(client, userdata, flag, rc):
 
 print("Connection to Broker with Address " + str(MQTT_Broker) + ":" + str(MQTT_Port))
 print("==================================")
-print("\nSubscribe on Topic = " + MQTT_Topic + "\n")
 
 # Assign Event Callbacks
 mqttc.on_message = on_message
