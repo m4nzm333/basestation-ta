d822c97a32d3536f32d1bb42f3c203da0c36634c
m4nzm333 <irman.mashuri@gmail.com>
2020-03-21 03:36:23 +0800

    Datalog : Get Array for Last Data in Directory

--- Datalog.py
+++ Datalog.py
@@ -24,6 +24,35 @@ class Datalog:
         dirLoc = "./data/subscriber/{}/{}/{}/{}/{}".format(now.year, now.month, now.day, now.hour, now.minute)
         Datalog.createDirDate(now)
 
-        file = open("{}/{}txt".format(dirLoc, now.strftime("%Y-%m-%d %H:%M:%S")), "a")
+        file = open("{}/{}.txt".format(dirLoc, now.strftime("%Y-%m-%d %H:%M:%S")), "a")
         file.write(data+'\n')
         file.close()
+    
+    # Get oldest file directory location
+    @staticmethod
+    def getDirLastData():
+        try:
+            files = os.listdir('./data/subscriber')
+            year = os.listdir('./data/subscriber/{}'.format(files[0]))
+            year.sort()
+            month = os.listdir('./data/subscriber/{}/{}'.format(files[0], year[0]))
+            month.sort()
+            day = os.listdir('./data/subscriber/{}/{}/{}'.format(files[0], year[0], month[0]))
+            day.sort()
+            hour = os.listdir('./data/subscriber/{}/{}/{}/{}'.format(files[0], year[0], month[0], day[0]))
+            hour.sort()
+            minute = os.listdir('./data/subscriber/{}/{}/{}/{}/{}'.format(files[0], year[0], month[0], day[0], hour[0]))
+            minute.sort()
+
+            return './data/subscriber/{}/{}/{}/{}/{}/{}'.format(files[0], year[0], month[0], day[0], hour[0], minute[0])
+        except:
+            return False
+    
+    # Get array the oldest file content
+    @staticmethod
+    def getArrayLastData():
+        try:
+            lastFile = open(Datalog.getDirLastData(), "r")
+            return lastFile.readlines()
+        except:
+            return []
