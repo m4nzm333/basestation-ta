dd22c88b9dd0f34a7d28d2e77d6cb4ae16fdf930
m4nzm333 <irman.mashuri@gmail.com>
2020-03-23 15:05:34 +0800

    SensorBME280: static method

--- ReadSensorBME280.py
+++ ReadSensorBME280.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2020 m4nzm333
 # Source code for read sensor data from BME280 Module
-#   * Temperature
-#   * Pressure
-#   * Hummidity
+#   * Temperature : Celcius
+#   * Pressure : hPa (HectoPascal or 100 Pascal)
+#   * Hummidity : %
 # Source : https://github.com/m4nzm333/basestation-ta
 # Ask a question, please contact:
 # e-mail    : irman.mashuri@gmail.com
@@ -12,24 +12,35 @@ import bme280
 
 class SensorBME280:
 
-    # Address I2C
-    port = 1
-    address = 0x76
-    bus = smbus2.SMBus(port)
-    # Get Data from Sensor
-    calibration_params = bme280.load_calibration_params(bus, address)
-
     @staticmethod
-    def getTemperature(self):
-        data = bme280.sample(self.bus, self.address, self.calibration_params)
+    def getTemperature():
+        # Address I2C
+        port = 1
+        address = 0x76
+        bus = smbus2.SMBus(port)
+        # Set Callibration
+        calibration_params = bme280.load_calibration_params(bus, address)
+        data = bme280.sample(bus, address, calibration_params)
         return data.temperature
 
     @staticmethod
-    def getPressure(self):
-        data = bme280.sample(self.bus, self.address, self.calibration_params)
+    def getPressure():
+        # Address I2C
+        port = 1
+        address = 0x76
+        bus = smbus2.SMBus(port)
+        # Set Callibration
+        calibration_params = bme280.load_calibration_params(bus, address)
+        data = bme280.sample(bus, address, calibration_params)
         return data.pressure
-        
+
     @staticmethod
-    def getHummidity(self):
-        data = bme280.sample(self.bus, self.address, self.calibration_params)
+    def getHummidity():
+        # Address I2C
+        port = 1
+        address = 0x76
+        bus = smbus2.SMBus(port)
+        # Set Callibration
+        calibration_params = bme280.load_calibration_params(bus, address)
+        data = bme280.sample(bus, address, calibration_params)
         return data.humidity
